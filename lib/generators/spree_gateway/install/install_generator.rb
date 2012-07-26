f63f74a015ef84838b33ddb480104b2de4168d71
Ryan Bigg <radarlistener@gmail.com>
2012-07-27 09:46:20 +1000

    spree_gateway does not provide any assets
    Fixes #24

--- lib/generators/spree_gateway/install/install_generator.rb
+++ lib/generators/spree_gateway/install/install_generator.rb
@@ -2,16 +2,6 @@ module SpreeGateway
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_gateway\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_gateway\n"
-      end
-
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_gateway\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_gateway\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_gateway'
       end
