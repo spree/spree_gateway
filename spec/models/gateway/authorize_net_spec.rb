d6c2fb46be15d33e6e9305523380dc701157982b
Jeff Dutil <jdutil21@gmail.com>
2012-09-04 11:10:58 -0400

    Add development dependencies.

--- spec/models/gateway/authorize_net_spec.rb
+++ spec/models/gateway/authorize_net_spec.rb
@@ -4,13 +4,13 @@ describe Spree::Gateway::AuthorizeNet do
   let (:gateway) { Spree::Gateway::AuthorizeNet.new }
 
   describe "options" do
-    it "should include :test => true in  when :test_mode is true" do
-      gateway.prefers_test_mode = true
+    it "should include :test => true when :test_mode is true" do
+      gateway.preferred_test_mode = true
       gateway.options[:test].should == true
     end
 
     it "should not include :test when test_mode is false" do
-      gateway.prefers_test_mode = false
+      gateway.preferred_test_mode = false
       gateway.options[:test].should be_nil
     end
   end
