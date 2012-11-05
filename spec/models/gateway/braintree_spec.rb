76af2b74072a2bccda089dde5d32044c06df787a
Ryan Bigg <radarlistener@gmail.com>
2012-11-05 15:31:58 +1100

    Fix class references in braintree_gateway_spec

--- spec/models/gateway/braintree_spec.rb
+++ spec/models/gateway/braintree_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe Spree::Gateway::Braintree do
+describe Spree::Gateway::BraintreeGateway do
 
   before(:each) do
     Spree::Gateway.update_all :active => false
-    @gateway = Spree::Gateway::Braintree.create!(:name => "Braintree Gateway", :environment => "test", :active => true)
+    @gateway = Spree::Gateway::BraintreeGateway.create!(:name => "Braintree Gateway", :environment => "test", :active => true)
 
     @gateway.set_preference(:merchant_id, "zbn5yzq9t7wmwx42" )
     @gateway.set_preference(:public_key, "ym9djwqpkxbv3xzt")
@@ -178,14 +178,14 @@ describe Spree::Gateway::Braintree do
   end
 
   def with_payment_profiles_off(&block)
-    Spree::Gateway::Braintree.class_eval do
+    Spree::Gateway::BraintreeGateway.class_eval do
       def payment_profiles_supported?
         false
       end
     end
     yield
   ensure
-    Spree::Gateway::Braintree.class_eval do
+    Spree::Gateway::BraintreeGateway.class_eval do
       def payment_profiles_supported?
         true
       end
