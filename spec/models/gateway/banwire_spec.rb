df5bfbdff98176439c45cb33ac57c2b58f022ab5
Amed Rodriguez <amed@tractical.com>
2012-11-13 17:49:32 -0600

    typo

--- spec/models/gateway/banwire_spec.rb
+++ spec/models/gateway/banwire_spec.rb
@@ -4,6 +4,6 @@ describe Spree::Gateway::Banwire do
   let(:gateway){ Spree::Gateway::Banwire.create!(name: "Banwire") }
 
   it "should be Banwire gateway" do
-    @gateway.provider_class.should == ::ActiveMerchant::Billing::BranwireGateway
+    @gateway.provider_class.should == ::ActiveMerchant::Billing::BanwireGateway
   end
 end
