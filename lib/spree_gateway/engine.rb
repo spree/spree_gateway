2fcfb3ffd147b1e74918656304649cea8377fabd
Chris Wise <cwise@murmurinformatics.com>
2013-01-21 08:04:44 -0500

    added support for BalancedPayments
    Fixes #59

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -22,6 +22,7 @@ module SpreeGateway
         app.config.spree.payment_methods << Spree::Gateway::Samurai
         app.config.spree.payment_methods << Spree::Gateway::Worldpay
         app.config.spree.payment_methods << Spree::Gateway::Banwire
+        app.config.spree.payment_methods << Spree::Gateway::Balanced
     end
   end
 
