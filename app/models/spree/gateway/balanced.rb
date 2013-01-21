2c9e850010e49e27d17e9aa484719c976c4b1800
Chris Wise <cwise@murmurinformatics.com>
2013-01-21 08:04:44 -0500

    added support for BalancedPayments
    Fixes #59

---
+++ app/models/spree/gateway/balanced.rb
@@ -0,0 +1,19 @@
+module Spree
+  class Gateway::Balanced < Gateway
+    preference :login, :string
+    preference :password, :string
+    preference :on_behalf_of_uri, :string
+
+    attr_accessible :preferred_login, :preferred_password, :preferred_on_behalf_of_uri
+    
+    def provider_class
+      ActiveMerchant::Billing::BalancedGateway
+    end
+
+    def options_with_test_preference
+      options_without_test_preference.merge(:test => self.preferred_test_mode)
+    end
+
+    alias_method_chain :options, :test_preference
+  end
+end
