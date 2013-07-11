69cc74b6ab02c10af3b67010c9e05a54139a815b
Ryan Bigg <radarlistener@gmail.com>
2013-07-11 15:53:25 +1000

    Add ActiveMerchant-backed USAEpay gateway
    Caveat:  Shopify/active_merchant#757 is outstanding. If you try to use this gateway in 'test' mode, it will not work until that PR has been merged.

---
+++ app/models/spree/gateway/usa_epay.rb
@@ -0,0 +1,20 @@
+module Spree
+  class Gateway::UsaEpay < Gateway
+    preference :login, :string
+    
+    attr_accessible :preferred_login
+
+    # Note: EWay supports purchase method only (no authorize method).
+    # Ensure Spree::Config[:auto_capture] is set to true
+
+    def provider_class
+      ActiveMerchant::Billing::UsaEpayGateway
+    end
+
+    def options_with_test_preference
+      options_without_test_preference.merge(:test => self.preferred_test_mode)
+    end
+
+    alias_method_chain :options, :test_preference
+  end
+end
