68758463a28cea89c3cba2ddc64e7a3f281505e0
Ryan Bigg <radarlistener@gmail.com>
2013-07-11 16:33:52 +1000

    Add Pin gateway

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -29,6 +29,7 @@ module SpreeGateway
         app.config.spree.payment_methods << Spree::Gateway::BalancedGateway
         app.config.spree.payment_methods << Spree::Gateway::DataCash
         app.config.spree.payment_methods << Spree::Gateway::UsaEpay
+        app.config.spree.payment_methods << Spree::Gateway::PinGateway
     end
   end
 
