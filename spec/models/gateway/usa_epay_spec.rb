ddd1ebf60af4759a2422b49aeef36a0014a9a747
Ryan Bigg <radarlistener@gmail.com>
2013-07-11 15:53:25 +1000

    Add ActiveMerchant-backed USAEpay gateway
    Caveat:  Shopify/active_merchant#757 is outstanding. If you try to use this gateway in 'test' mode, it will not work until that PR has been merged.
    
    Conflicts:
    	app/models/spree/gateway/usa_epay.rb

---
+++ spec/models/gateway/usa_epay_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Spree::Gateway::UsaEpay do
+
+  before(:each) do
+    Spree::Gateway.update_all :active => false
+    @gateway = Spree::Gateway::UsaEpay.create!(:name => "USA EPay Gateway", :environment => "sandbox", :active => true)
+
+    @gateway.set_preference(:login, "0r19zQBdp5nS8i3t4hFxz0di13yf56q1" )
+    @gateway.save!
+
+    @country = Factory(:country, :name => "United States", :iso_name => "UNITED STATES", :iso3 => "USA", :iso => "US", :numcode => 840)
+    @state   = Factory(:state, :name => "Maryland", :abbr => "MD", :country => @country)
+    @address = Factory(:address,
+      :firstname => 'John',
+      :lastname => 'Doe',
+      :address1 => '1234 My Street',
+      :address2 => 'Apt 1',
+      :city =>  'Washington DC',
+      :zipcode => '20123',
+      :phone => '(555)555-5555',
+      :state => @state,
+      :country => @country
+    )
+    @order = Factory(:order_with_totals, :bill_address => @address, :ship_address => @address)
+    @order.update!
+    @credit_card = Factory(:credit_card, :verification_value => '123', :number => '4111111111111111', :month => 9, :year => Time.now.year + 1, :first_name => 'John', :last_name => 'Doe')
+    @payment = Factory(:payment, :source => @credit_card, :order => @order, :payment_method => @gateway, :amount => 10.00)
+    @payment.payment_method.environment = "test"
+  end
+
+  describe "purchasing" do
+    it "can purchase a payment" do
+      @payment.purchase!.should be_true
+    end
+  end
+end
