e4fb35cdfb39a1a97a281c64ab00026800411485
Brandon Bradley <crixdev@gmail.com>
2013-08-06 18:30:59 -0500

    Add PayJunction gateway and tests
    Fixes #92

---
+++ app/models/spree/gateway/pay_junction.rb
@@ -0,0 +1,18 @@
+module Spree
+  class Gateway::PayJunction < Gateway
+    preference :login, :string
+    preference :password, :string
+
+    attr_accessible :preferred_login, :preferred_password
+
+    def provider_class
+      ActiveMerchant::Billing::PayJunctionGateway
+    end
+
+    def options_with_test_preference
+      options_without_test_preference.merge(:test => self.preferred_test_mode)
+    end
+
+    alias_method_chain :options, :test_preference
+  end
+end
---
+++ spec/models/gateway/pay_junction_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Spree::Gateway::PayJunction do
+  let (:gateway) { Spree::Gateway::PayJunction.create!(:name => "PayJunction") }
+
+  describe "options" do
+    it "should include :test => true in  when :test_mode is true" do
+      gateway.preferred_test_mode = true
+      gateway.options[:test].should == true
+    end
+
+    it "should not include :test when test_mode is false" do
+      gateway.preferred_test_mode = false
+      gateway.options[:test].should == false
+    end
+  end
+end
