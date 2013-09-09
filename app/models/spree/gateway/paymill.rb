f015c108effec3eca13b567fb44a73203c4af88e
Ryan Bigg <radarlistener@gmail.com>
2013-09-09 12:13:40 +1000

    Remove attr_accessible call from Paymill gateway

--- app/models/spree/gateway/paymill.rb
+++ app/models/spree/gateway/paymill.rb
@@ -5,9 +5,6 @@ module Spree
     preference :private_key, :string
     preference :currency, :string, :default => 'GBP'
 
-    attr_accessible :preferred_public_key, :preferred_private_key, :preferred_currency
-
-
     def provider_class
       ActiveMerchant::Billing::PaymillGateway
     end
