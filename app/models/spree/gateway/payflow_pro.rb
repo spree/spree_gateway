0ac05b48851cedbb5361b03b50573b5c576bbb51
John Hirbour <john@hirbour.org>
2013-10-18 16:35:12 -0400

    payflow pro support
    Fixes #108

---
+++ app/models/spree/gateway/payflow_pro.rb
@@ -0,0 +1,17 @@
+module Spree
+  class Gateway::PayflowPro < Gateway
+    preference :login, :string
+    preference :password, :password
+
+    def provider_class
+      ActiveMerchant::Billing::PayflowGateway
+    end
+
+    def options_with_test_preference
+      options_without_test_preference.merge(:test => self.preferred_test_mode)
+    end
+
+    alias_method_chain :options, :test_preference
+
+  end
+end
