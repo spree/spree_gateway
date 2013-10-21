5a8b00ec6ec2a4f0067d7ff0bbf473b70e2dad83
Ryan Bigg <radarlistener@gmail.com>
2013-10-21 11:48:32 +1100

    Use add_routes to add routes to Core engine
    prepending the routes was causing problems as seen in rails/rails#11895.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.add_routes do
   # Add your extension routes here
   resources :orders do
     resource :checkout, :controller => 'checkout' do
