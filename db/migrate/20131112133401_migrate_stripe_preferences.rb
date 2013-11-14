3d5ff2e66d2f8f2056ab6df2c8ddde1efdd306cf
Ryan Bigg <radarlistener@gmail.com>
2013-11-14 15:04:08 +1100

    Quote the 'key' column so that MySQL doesn't spit the dummy

--- db/migrate/20131112133401_migrate_stripe_preferences.rb
+++ db/migrate/20131112133401_migrate_stripe_preferences.rb
@@ -1,6 +1,6 @@
 class MigrateStripePreferences < ActiveRecord::Migration
   def up
-    Spree::Preference.where("key LIKE 'spree/gateway/stripe_gateway/login%'").each do |pref|
+    Spree::Preference.where("#{ActiveRecord::Base.connection.quote_column_name("key")} LIKE \"spree/gateway/stripe_gateway/login%\"").each do |pref|
       pref.key = pref.key.gsub('login', 'secret_key')
       pref.save
     end
