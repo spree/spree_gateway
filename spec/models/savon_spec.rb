2f0db75d111b9a053620cda92530cdfa5468a77c
Michał Szajbe <michal.szajbe@gmail.com>
2014-03-12 16:04:39 +0100

    Removed savon dependency
    Fixes #139

--- spec/models/savon_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Savon do
-  let(:config) { Savon.config }
-
-  it 'does not log anything' do
-    expect(Savon.config.logger).to be_a_kind_of Savon::NullLogger
-  end
-end
