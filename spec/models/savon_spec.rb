c8315251619f5c52644b3887e84387c580a0f2bc
Michał Szajbe <michal.szajbe@gmail.com>
2014-03-12 16:04:39 +0100

    Removed savon dependency
    Fixes #139
    
    Conflicts:
    	lib/spree_gateway.rb
    	spec/models/savon_spec.rb
    	spree_gateway.gemspec

--- spec/models/savon_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Savon do
-  let(:config) { Savon.config }
-
-  it "should not log anything" do
-    Savon.config.logger.should be_a_kind_of(Savon::NullLogger)
-  end
-end
