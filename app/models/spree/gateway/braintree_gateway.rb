24c8935fe18483ffdb7d59eeaca651bcbdb1f031
Ryan Bigg <radarlistener@gmail.com>
2014-04-30 15:19:58 +1000

    Add BraintreeGateway#cancel
    This new method will decide whether to void or to refund a payment.
    
    A payment can be voided if it's either authorized or submitted for settlement. In all other states, it MUST be refunded.
    
    Fixes #145
    
    Conflicts:
    	app/models/spree/gateway/braintree_gateway.rb

--- app/models/spree/gateway/braintree_gateway.rb
+++ app/models/spree/gateway/braintree_gateway.rb
@@ -118,6 +118,18 @@ module Spree
       h
     end
 
+    def cancel(response_code)
+      transaction = ::Braintree::Transaction.find(response_code)
+      # From: https://www.braintreepayments.com/docs/ruby/transactions/refund
+      # "A transaction can be refunded if its status is settled or settling.
+      # If the transaction has not yet begun settlement, it should be voided instead of refunded.
+      if transaction.status == Braintree::Transaction::Status::SubmittedForSettlement
+        provider.void(response_code)
+      else
+        provider.refund(response_code)
+      end
+    end
+
     def preferences
       preferences = super.slice(:merchant_id,
                                 :merchant_account_id,
