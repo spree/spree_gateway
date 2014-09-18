4b43cd8546d36fbf632887fe613c48fd04007e7b
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-09-18 16:23:29 -0400

    Pass options to all methods, and cleanup some syntax.

--- app/models/spree/gateway/authorize_net_cim.rb
+++ app/models/spree/gateway/authorize_net_cim.rb
@@ -35,15 +35,15 @@ module Spree
     # capture is only one where source is not passed in for payment profile
     def capture(amount, response_code, gateway_options)
       # no credit card needed
-      create_transaction(amount, nil, :prior_auth_capture, :trans_id => response_code)
+      create_transaction(amount, nil, :prior_auth_capture, transaction_options(gateway_options).merge(trans_id: response_code))
     end
 
     def credit(amount, creditcard, response_code, gateway_options)
-      create_transaction(amount, creditcard, :refund, :trans_id => response_code)
+      create_transaction(amount, creditcard, :refund, transaction_options(gateway_options).merge(trans_id: response_code))
     end
 
     def void(response_code, creditcard, gateway_options)
-      create_transaction(nil, creditcard, :void, :trans_id => response_code)
+      create_transaction(nil, creditcard, :void, transaction_options(gateway_options).merge(trans_id: response_code))
     end
 
     def payment_profiles_supported?
@@ -54,7 +54,7 @@ module Spree
     def create_profile(payment)
       if payment.source.gateway_customer_profile_id.nil?
         profile_hash = create_customer_profile(payment)
-        payment.source.update_attributes(:gateway_customer_profile_id => profile_hash[:customer_profile_id], :gateway_payment_profile_id => profile_hash[:customer_payment_profile_id])
+        payment.source.update_attributes(gateway_customer_profile_id: profile_hash[:customer_profile_id], gateway_payment_profile_id: profile_hash[:customer_payment_profile_id])
       end
     end
 
@@ -62,7 +62,7 @@ module Spree
     def get_profile(payment)
       if payment.source.has_payment_profile?
         cim_gateway.get_customer_profile({
-          :customer_profile_id => payment.source.gateway_customer_profile_id
+          customer_profile_id: payment.source.gateway_customer_profile_id
         }).params['profile'].deep_symbolize_keys!
       end
     end
@@ -85,7 +85,6 @@ module Spree
         if hash[:payment][:credit_card]
           # activemerchant expects a credit card object with 'number', 'year', 'month', and 'verification_value?' defined
           payment.source.define_singleton_method(:number) { "XXXXXXXXX#{payment.source.last_digits}" }
-          puts payment.source.number
           hash[:payment][:credit_card] = payment.source
         end
         cim_gateway.update_customer_payment_profile({
@@ -98,7 +97,7 @@ module Spree
     private
 
       def transaction_options(gateway_options)
-        { :order => { :invoice_number => gateway_options[:order_id] } }
+        { order: { invoice_number: gateway_options[:order_id] } }
       end
 
       # Create a transaction on a creditcard
@@ -108,19 +107,19 @@ module Spree
         creditcard.save if creditcard
 
         transaction_options = {
-          :type => transaction_type
+          type: transaction_type
         }.update(options)
 
         if amount
           amount = "%.2f" % (amount / 100.0) # This gateway requires formated decimal, not cents
           transaction_options.update({
-            :amount => amount
+            amount: amount
           })
         end
 
         transaction_options.update({
-          :customer_profile_id => creditcard.gateway_customer_profile_id,
-          :customer_payment_profile_id => creditcard.gateway_payment_profile_id
+          customer_profile_id: creditcard.gateway_customer_profile_id,
+          customer_payment_profile_id: creditcard.gateway_payment_profile_id
         }) if creditcard
 
         logger.debug("\nAuthorize Net CIM Request")
@@ -136,8 +135,8 @@ module Spree
         options = options_for_create_customer_profile(payment)
         response = cim_gateway.create_customer_profile(options)
         if response.success?
-          { :customer_profile_id => response.params['customer_profile_id'],
-            :customer_payment_profile_id => response.params['customer_payment_profile_id_list'].values.first }
+          { customer_profile_id: response.params['customer_profile_id'],
+            customer_payment_profile_id: response.params['customer_payment_profile_id_list'].values.first }
         else
           payment.send(:gateway_error, response)
         end
@@ -145,25 +144,35 @@ module Spree
 
       def options_for_create_customer_profile(payment)
         if payment.is_a? CreditCard
-          info = { :bill_to => generate_address_hash(payment.address), :payment => { :credit_card => payment } }
+          info = { bill_to: generate_address_hash(payment.address),
+                   payment: { credit_card: payment } }
         else
-          info = { :bill_to => generate_address_hash(payment.order.bill_address),
-                   :payment => { :credit_card => payment.source } }
+          info = { bill_to: generate_address_hash(payment.order.bill_address),
+                   payment: { credit_card: payment.source } }
         end
         validation_mode = preferred_validate_on_profile_create ? preferred_server.to_sym : :none
 
-        { :profile => { :merchant_customer_id => "#{Time.now.to_f}",
-                        :ship_to_list => generate_address_hash(payment.order.ship_address),
-                        :email => payment.order.email,
-                        :payment_profiles => info },
-          :validation_mode => validation_mode }
+        { profile: { merchant_customer_id: "#{Time.now.to_f}",
+                     ship_to_list: generate_address_hash(payment.order.ship_address),
+                     email: payment.order.email,
+                     payment_profiles: info },
+          validation_mode: validation_mode }
       end
 
       # As in PaymentGateway but with separate name fields
       def generate_address_hash(address)
         return {} if address.nil?
-        {:first_name => address.firstname, :last_name => address.lastname, :address1 => address.address1, :address2 => address.address2, :city => address.city,
-         :state => address.state_text, :zip => address.zipcode, :country => address.country.iso, :phone_number => address.phone}
+        {
+          first_name: address.firstname,
+          last_name: address.lastname,
+          address1: address.address1,
+          address2: address.address2,
+          city: address.city,
+          state: address.state_text,
+          zip: address.zipcode,
+          country: address.country.iso,
+          phone_number: address.phone
+        }
       end
 
       def cim_gateway
@@ -172,5 +181,6 @@ module Spree
         gateway_options[:test_requests] = false # DD: never ever do test requests because just returns transaction_id = 0
         ActiveMerchant::Billing::AuthorizeNetCimGateway.new(gateway_options)
       end
+
   end
 end
