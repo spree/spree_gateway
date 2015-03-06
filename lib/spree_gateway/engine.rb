7931f28e61d4cdbcb5d267e9f817911a83839c01
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-03-06 16:27:40 -0500

    LinkPoint no longer exists. Fixes #213

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -17,7 +17,6 @@ module SpreeGateway
       app.config.spree.payment_methods << Spree::Gateway::DataCash
       app.config.spree.payment_methods << Spree::Gateway::Epay
       app.config.spree.payment_methods << Spree::Gateway::Eway
-      app.config.spree.payment_methods << Spree::Gateway::Linkpoint
       app.config.spree.payment_methods << Spree::Gateway::Maxipago
       app.config.spree.payment_methods << Spree::Gateway::Migs
       app.config.spree.payment_methods << Spree::Gateway::Moneris
