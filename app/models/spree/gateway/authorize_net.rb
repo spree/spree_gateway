dd81dff539107a00097ce16c692b7656c739cc03
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-03-18 12:29:49 -0400

    The response code saved from Auth.net doesnt have required card_number appended with #.
    For AuthorizeNetGateway the refund action attempts to lookup card_number from splitting authorization:
    https://github.com/Shopify/active_merchant/blob/master/lib/active_merchant/billing/gateways/authorize_net.rb#L107
    
    It attempts to split response_code#card_number:
    https://github.com/Shopify/active_merchant/blob/master/lib/active_merchant/billing/gateways/authorize_net.rb#L478
    
    However #card_number is not appended to response_code so we must set it as a fallback option:
    https://github.com/Shopify/active_merchant/blob/master/lib/active_merchant/billing/gateways/authorize_net.rb#L114
    
    Fixes spree/spree#4813

--- app/models/spree/gateway/authorize_net.rb
+++ app/models/spree/gateway/authorize_net.rb
@@ -10,7 +10,33 @@ module Spree
     def options_with_test_preference
       options_without_test_preference.merge(test: self.preferred_test_mode)
     end
-
     alias_method_chain :options, :test_preference
+
+    def credit(amount, response_code, refund, gateway_options = {})
+      gateway_options[:card_number] = refund[:originator].payment.source.last_digits
+      auth_net_gateway.refund(amount, response_code, gateway_options)
+    end
+
+    private
+
+    def auth_net_gateway
+      @_auth_net_gateway ||= begin
+        ActiveMerchant::Billing::Base.gateway_mode = preferred_server.to_sym
+        gateway_options = options
+        gateway_options[:test_requests] = false # DD: never ever do test requests because just returns transaction_id = 0
+        ActiveMerchant::Billing::AuthorizeNetGateway.new(gateway_options)
+      end
+    end
+
+    def options
+      # add :test key in the options hash, as that is what the ActiveMerchant::Billing::AuthorizeNetGateway expects
+      if self.preferred_test_mode
+        self.preferences[:test] = true
+      else
+        self.preferences.delete(:test)
+      end
+
+      super
+    end
   end
 end
