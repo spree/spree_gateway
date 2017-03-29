dd673b9a78783673d81e70338b92750c39c0257d
mizzlala3 <leiren03@me.com>
2017-03-28 23:23:00 -0400

    More edits

--- app/models/spree/gateway/authorize_net.rb
+++ app/models/spree/gateway/authorize_net.rb
@@ -2,7 +2,14 @@ module Spree
   class Gateway::AuthorizeNet < Gateway
     preference :login, :string
     preference :password, :string
-    preference :server, :string, default: "test"
+    preference :server, :string, :default => "live"
+
+       # WARNING: Unlike other payment integrations, 'test mode' in Authorize Net's termiminology does not mean use test server
+       # instead, use preferred server set to either 'live' or 'test'
+       # DO NOT TURN TEST MODE TO ON (EVEN IN QA/STAGING ENVIRONMENTS), it will return 0 as transaction ids under all circumstances
+
+       # here, it overloads the setting on the base class, which confusingly defaults to true
+       preference :test_mode, :boolean, :default => false
 
     def provider_class
       ActiveMerchant::Billing::AuthorizeNetGateway
@@ -10,9 +17,10 @@ module Spree
 
     def options_with_test_preference
       if !['live','test'].include?(self.preferred_server)
-        raise "You must set the 'server' preference in your payment method (Gateway::AuthorizeNet) to either 'live' or 'test'"
-      end
-      options_without_test_preference.merge(test: (self.preferred_server != "live") )
+    ActiveSupport::Deprecation.warn("You must set your preferred server to either 'live' or 'test'")
+  end
+  # warning: 'test' parameter indicates live or test server; it DOES NOT indicate Authorize.net test-mode
+  options_without_test_preference.merge(:test => (self.preferred_server != "live") )
     end
 
     def cancel(response_code)
--- app/models/spree/gateway/payflow_pro.rb
+++ app/models/spree/gateway/payflow_pro.rb
@@ -3,13 +3,24 @@ module Spree
     preference :login, :string
     preference :password, :password
     preference :partner, :string
+    preference :server, :string, :default => "live"
 
+       # WARNING: Unlike other payment integrations, 'test mode' in Authorize Net's termiminology does not mean use test server
+       # instead, use preferred server set to either 'live' or 'test'
+       # DO NOT TURN TEST MODE TO ON (EVEN IN QA/STAGING ENVIRONMENTS), it will return 0 as transaction ids under all circumstances
+
+       # here, it overloads the setting on the base class, which confusingly defaults to true
+       preference :test_mode, :boolean, :default => false
     def provider_class
       ActiveMerchant::Billing::PayflowGateway
     end
 
     def options_with_test_preference
-      options_without_test_preference.merge(:test => self.preferred_test_mode)
+      if !['live','test'].include?(self.preferred_server)
+    ActiveSupport::Deprecation.warn("You must set your preferred server to either 'live' or 'test'")
+  end
+  # warning: 'test' parameter indicates live or test server; it DOES NOT indicate Authorize.net test-mode
+  options_without_test_preference.merge(:test => (self.preferred_server != "live") )
     end
 
     alias_method_chain :options, :test_preference
