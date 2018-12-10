5c84d92d475586a6478c49011af2bdaf69df9ca8
Damian Legawiec <damian@sparksolutions.co>
2018-12-10 14:15:14 +0100

    Better handling of non existing Apple Pay Payment Gateway in Apple Pay Domain Verification endpoint

--- lib/controllers/spree/apple_pay_domain_verification_controller.rb
+++ lib/controllers/spree/apple_pay_domain_verification_controller.rb
@@ -3,6 +3,8 @@ module Spree
     def show
       gateway = Spree::Gateway::StripeApplePayGateway.active.last
 
+      raise ActiveRecord::RecordNotFound unless gateway
+
       render plain: gateway.preferred_domain_verification_certificate
     end
   end
