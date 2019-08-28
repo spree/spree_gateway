e6a6b5afdc51e975e80cbb1048752cf46417d99a
Damian Legawiec <damian@sparksolutions.co>
2019-08-28 16:14:46 +0200

    Fix decorators to work with Rails 6.0 and Zeitwerk autoloader

---
+++ app/models/spree/apple_pay_order_decorator.rb
@@ -0,0 +1,20 @@
+module Spree
+  module ApplePayOrderDecorator
+    def confirmation_required?
+      return false if paid_with_apple_pay?
+
+      # Little hacky fix for #4117
+      # If this wasn't here, order would transition to address state on confirm failure
+      # because there would be no valid payments any more.
+      Spree::Config[:always_include_confirm_step] ||
+        payments.valid.map(&:payment_method).compact.any?(&:payment_profiles_supported?) ||
+        confirm?
+    end
+
+    def paid_with_apple_pay?
+      payments.valid.any?(&:apple_pay?)
+    end
+  end
+end
+
+Spree::Order.prepend Spree::ApplePayOrderDecorator
