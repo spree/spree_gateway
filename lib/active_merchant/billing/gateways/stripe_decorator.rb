a32ef41c96028d33eee36b166a2a71876c6f2d47
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-11-27 13:34:19 +0100

    Corrects stripe gateway decorator

---
+++ lib/active_merchant/billing/gateways/stripe_decorator.rb
@@ -0,0 +1,29 @@
+module ActiveMerchant
+  module Billing
+    module Gateways
+      module StripeDecorator
+        def self.prepended(base)
+          base.class_eval do
+            alias_method :original_headers, :headers
+            alias_method :original_add_customer_data, :add_customer_data
+
+            private
+
+            def headers(options = {})
+              headers = original_headers(options)
+              headers['User-Agent'] = headers['X-Stripe-Client-User-Agent']
+              headers
+            end
+
+            def add_customer_data(post, options)
+              original_add_customer_data(post, options)
+              post[:payment_user_agent] = "SpreeGateway/#{SpreeGateway.version}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+ActiveMerchant::Billing::Gateways::Stripe.prepend(ActiveMerchant::Billing::Gateways::StripeDecorator)
