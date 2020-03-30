d3b0bbce8ce3c65cbfe7908d3237b6f4f0adb752
piotrpanko <piotr.panko@sparksolutions.co>
2020-03-30 18:28:05 +0200

    Add actions to the payment method

--- app/models/spree/check.rb
+++ app/models/spree/check.rb
@@ -13,5 +13,24 @@ module Spree
     def has_payment_profile?
       gateway_customer_profile_id.present? || gateway_payment_profile_id.present?
     end
+
+    def actions
+      %w{capture void credit}
+    end
+
+    def can_capture?(payment)
+      payment.pending? || payment.checkout?
+    end
+
+    # Indicates whether its possible to void the payment.
+    def can_void?(payment)
+      !payment.failed? && !payment.void?
+    end
+
+    # Indicates whether its possible to credit the payment.  Note that most gateways require that the
+    # payment be settled first which generally happens within 12-24 hours of the transaction.
+    def can_credit?(payment)
+      payment.completed? && payment.credit_allowed > 0
+    end
   end
 end
