7da13b459f91aeb786c47769cdc42b7247053713
chhga <oleg.leontiev@sparksolutions.co>
2020-04-22 19:13:48 +0200

    Update db/migrate/20200422114908_add_intent_key_to_payment.rb
    Co-Authored-By: Damian Legawiec <damian@sparksolutions.co>

--- db/migrate/20200422114908_add_intent_key_to_payment.rb
+++ db/migrate/20200422114908_add_intent_key_to_payment.rb
@@ -1,4 +1,4 @@
-class AddIntentKeyToPayment < ActiveRecord::Migration[6.0]
+class AddIntentKeyToPayment < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_payments, :intent_client_key, :string
   end
