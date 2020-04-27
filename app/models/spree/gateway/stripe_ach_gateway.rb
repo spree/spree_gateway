14850de6a5e81ee8fb495457e57b016f68684833
piotrpanko <piotr.panko@sparksolutions.co>
2020-04-27 17:11:16 +0200

    Add conditionals for the order check, add ACH to the engine

--- app/models/spree/gateway/stripe_ach_gateway.rb
+++ app/models/spree/gateway/stripe_ach_gateway.rb
@@ -48,7 +48,12 @@ module Spree
     def available_for_order?(order)
       # Stripe ACH payments are supported only for US customers
       # Therefore we need to check order's addresses
-      usa_id = ::Spree::Country.find_by(iso: 'US').id
+      return unless order.ship_address_id && order.bill_address_id
+      return unless order.ship_address && order.bill_address_id
+
+      usa_id = ::Spree::Country.find_by(iso: 'US')&.id
+      return false unless usa_id
+
       order.ship_address.country_id == usa_id && order.bill_address.country_id == usa_id
     end
   end
