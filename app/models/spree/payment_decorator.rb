a54cd31f53bb56d8007087e5d9e5a2299fc2c5ce
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2020-04-28 16:12:09 +0200

    [SD-852] spec

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,8 +1,7 @@
 module Spree
   module PaymentDecorator
     def handle_response(response, success_state, failure_state)
-      self.intent_client_key = response.params['client_secret'] if response.params['client_secret'] && response.success?
-
+      self.intent_client_key = response.params['client_secret'] if response.params['client_secret']
       super
     end
   end
