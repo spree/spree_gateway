a4c34bffeb5616d2c22d57e4cfd4722c2eeb10ea
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2020-05-04 14:26:33 +0200

    [SD-852] change the way of adding step

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,17 +1,12 @@
 module Spree
   module OrderDecorator
     def self.prepended(base)
-      base.checkout_flow do
-        go_to_state :address
-        go_to_state :delivery
-        go_to_state :payment, if: ->(order) { order.payment? || order.payment_required? }
-        go_to_state :confirm, if: ->(order) { order.confirmation_required? }
-        go_to_state :confirm_payment, if: ->(order) { order.payments.map { |p| p.payment_method.has_preference?(:intents) && p.payment_method.get_preference(:intents) }.any? }
-        go_to_state :complete
-        remove_transition from: :delivery, to: :confirm, unless: ->(order) { order.confirmation_required? }
-      end
+      base.insert_checkout_step(:payment_confirm, after: :confirm, if: ->(order) { order.payments.map { |p| p.payment_method.has_preference?(:intents) && p.payment_method.get_preference(:intents) }.any? }) unless base.checkout_steps.has_key?(:payment_confirm)
+
+      base.state_machine.before_transition to: :payment_confirm, do: ->(order) { order.process_payments! }
     end
   end
 end
 
 Spree::Order.prepend(Spree::OrderDecorator)
+
