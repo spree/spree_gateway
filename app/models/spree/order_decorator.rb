83d6414dd4cd8f2b6946ea9a0c6a2ba9a271a2c6
Szymon Iwacz <szymon@iwacz.pl>
2020-09-14 13:05:35 +0200

    [SD-852] Rubocop fixes

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,12 +1,12 @@
 module Spree
   module OrderDecorator
     def self.prepended(base)
-      return if base.checkout_steps.has_key?(:payment_confirm)
+      return if base.checkout_steps.key?(:payment_confirm)
 
       base.insert_checkout_step(
         :payment_confirm,
         after: :payment,
-        if: ->(order) do
+        if: lambda do |order|
           order.payments.valid.map { |p| p.payment_method&.has_preference?(:intents) && p.payment_method&.get_preference(:intents) }.any?
         end
       )
