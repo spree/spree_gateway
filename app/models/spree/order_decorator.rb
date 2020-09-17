dde3911f23e57329fdb19140c84693d6074e7159
Szymon Iwacz <szymon@iwacz.pl>
2020-09-17 17:03:31 +0200

    [SD-852] Add condition to process_payments! for intents presence

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -6,9 +6,7 @@ module Spree
       base.insert_checkout_step(
         :payment_confirm,
         before: :complete,
-        if: lambda do |order|
-          order.payments.valid.map { |p| p.payment_method&.has_preference?(:intents) && p.payment_method&.get_preference(:intents) }.any?
-        end
+        if: ->(order) { order.intents? }
       )
     end
 
@@ -16,9 +14,13 @@ module Spree
       # Payments are processed in confirm_payment step where after successful
       # 3D Secure authentication `intent_client_key` is saved for payment.
       # In case authentication is unsuccessful, `intent_client_key` is removed.
-      return unless payments.last.intent_client_key.nil?
+      return if intents? && payments.valid.last.intent_client_key.present?
 
       super
+    end
+
+    def intents?
+      payments.valid.map { |p| p.payment_method&.has_preference?(:intents) && p.payment_method&.get_preference(:intents) }.any?
     end
   end
 end
