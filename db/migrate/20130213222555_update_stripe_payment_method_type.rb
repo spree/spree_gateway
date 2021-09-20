345a026dc453d5ceb1265dcbf40676ee56a729a3
Damian Legawiec <damian@sparksolutions.co>
2021-09-20 22:48:29 +0200

    Remove ancient migrations updating records

--- db/migrate/20130213222555_update_stripe_payment_method_type.rb
+++
@@ -1,9 +0,0 @@
-class UpdateStripePaymentMethodType < SpreeExtension::Migration[4.2]
-  def up
-    Spree::PaymentMethod.where(:type => "Spree::Gateway::Stripe").update_all(:type => "Spree::Gateway::StripeGateway")
-  end
-
-  def down
-    Spree::PaymentMethod.where(:type => "Spree::Gateway::StripeGateway").update_all(:type => "Spree::Gateway::Stripe")
-  end
-end
--- db/migrate/20130415222802_update_balanced_payment_method_type.rb
+++
@@ -1,9 +0,0 @@
-class UpdateBalancedPaymentMethodType < SpreeExtension::Migration[4.2]
-  def up
-    Spree::PaymentMethod.where(:type => "Spree::Gateway::Balanced").update_all(:type => "Spree::Gateway::BalancedGateway")
-  end
-
-  def down
-    Spree::PaymentMethod.where(:type => "Spree::Gateway::BalancedGateway").update_all(:type => "Spree::Gateway::Balanced")
-  end
-end
